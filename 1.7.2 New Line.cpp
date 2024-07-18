eaceebdae6a55e82a0f3a9fd4434f0dd31b32c9d
Elias V. <58016431+Eli-Pop@users.noreply.github.com>
2024-07-17 21:56:29 -0500

    Add files via upload
    C++ file show casing the new line function.

---
+++ 1.7.2 New Line.cpp
@@ -0,0 +1,21 @@
+/*
+* This program demonstrates the \n code, which
+* generates a new line. Have been using this
+* on previous codes.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "one\n";
+	cout << "two\n";
+	cout << "three";
+	cout << "four";
+
+	//Copied this code from the book. 
+	//Four is on purpose not going to have the new line code.
+	system("pause");
+	return 0;
+}
